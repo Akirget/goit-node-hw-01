8fcbd59b632e3c0a4e154243d44f44fc0245be8f
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-12-09 15:18:54 +0100

    Домашнє завдання №1 Node

--- contacts.js
+++ contacts.js
@@ -1,12 +1,61 @@
 const fs = require('fs/promises');
 const path = require('path');
+const { v4: uuid } = require('uuid');
 
 const contactsPath = path.join(__dirname, './db/contacts.json');
 
-async function listContacts() {}
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const contacts = JSON.parse(data);
 
-async function getContactById(contactId) {}
+    return contacts;
+  } catch (error) {
+    console.log(error.message);
+    return error;
+  }
+}
 
-async function removeContact(contactId) {}
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const contact = contacts.find(({ id }) => id === contactId);
+    return contact;
+  } catch (error) {
+    console.log(error.message);
+    return error;
+  }
+}
 
-async function addContact(name, email, phone) {}
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(({ id }) => id === contactId);
+    if (index === -1) {
+      throw new Error('contact not found');
+    }
+    const updateContacts = contacts.slice(index, 1);
+    return updateContacts;
+  } catch (error) {
+    console.log(error.message);
+    return error;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { name, email, phone, id: uuid() };
+    contacts.push(newContact);
+  } catch (error) {
+    console.log(error.message);
+    return error;
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
