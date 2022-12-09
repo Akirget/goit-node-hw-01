c77d6d880631338912649627cd123ec3c7e088ce
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-12-09 17:32:10 +0100

    Домашнє завдання №1 Node

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 
 node index.js --action="list"
 
-![1](https://i.ibb.co/LdVdYWF/1.png)
+![1](https://i.ibb.co/Hd8P2jt/1.png)
 
 # Отримуємо контакт по id
 
-![2](https://i.ibb.co/3sgjjRV/2.png)
+![2](https://i.ibb.co/StM3fTm/2.png)
+
+# Додаємо контакт
+
+![3](https://i.ibb.co/CJHktwJ/3.png)
+
+# Видаляємо контакт
+
+![4](https://i.ibb.co/8DbHVYL/4.png)
--- contacts.js
+++ contacts.js
@@ -16,7 +16,7 @@ async function listContacts() {
   try {
     const data = await fs.readFile(contactsPath, 'utf-8');
     const contacts = JSON.parse(data);
-    console.log(contacts);
+
     return contacts;
   } catch (error) {
     console.log(error.message);
@@ -39,13 +39,10 @@ async function getContactById(contactId) {
 async function removeContact(contactId) {
   try {
     const contacts = await listContacts();
-    const index = contacts.findIndex(({ id }) => id === contactId);
-    if (index === -1) {
-      throw new Error('contact not found');
-    }
-    const updateContacts = contacts.slice(index, 1);
-    updateFile();
-    return updateContacts;
+    const changeContacts = contacts.filter(({ id }) => id !== contactId);
+    const newListContact = contacts.filter(({ id }) => id === contactId);
+    updateFile(changeContacts);
+    return newListContact;
   } catch (error) {
     console.log(error.message);
     return error;
@@ -57,7 +54,7 @@ async function addContact(name, email, phone) {
     const contacts = await listContacts();
     const newContact = { name, email, phone, id: uuid() };
     contacts.push(newContact);
-    updateFile();
+    updateFile(contacts);
     return newContact;
   } catch (error) {
     console.log(error.message);
